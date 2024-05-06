879efd5c632ba6aa04af6ca66ff8dc87534d7115
Tatiana Kopczynska <kopczynska.tatiana@gmail.com>
2024-05-06 20:30:56 +0200

    Test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,24 @@
 #include "../src/code.hpp"
 
+#include <iostream>
 int main() {
-  // TODO: test code goes here
+
+  if (dodawanie(0, 0) != 0){
+    std::cerr << "Błąd: 0 + 0 != 0" << std::endl;
+    return 1;
+  }
+    if (odejmowanie(10, 5) != 5){
+    std::cerr << "Błąd: 10 - 5 != 5" << std::endl;
+    return 1;
+  }
+    if (mnozenie(2, 2) != 4){
+    std::cerr << "Błąd: 2 * 2 != 4" << std::endl;
+    return 1;
+  }
+    if (dzielenie(4, 2) != 2){
+    std::cerr << "Błąd: 4 / 2 != 2" << std::endl;
+    return 1;
+  }
+  std::cout << "Jest git!" << std::endl;
+  return 0;
 }
