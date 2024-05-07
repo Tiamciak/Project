9295084bcf8dbe82d9c6641b754b03177060c8cf
Tatiana Kopczynska <kopczynska.tatiana@gmail.com>
2024-05-07 20:44:17 +0200

    Revert to correct version

--- src/code.cpp
+++ src/code.cpp
@@ -1,20 +1,20 @@
 #include "code.hpp"
 
 double dodawanie(double num1, double num2) {
-    return num1 - num2;
-}
-
-double odejmowanie(double num1, double num2) {
     return num1 + num2;
 }
 
+double odejmowanie(double num1, double num2) {
+    return num1 - num2;
+}
+
 double mnozenie(double num1, double num2) {
-    return num1 / num2;
+    return num1 * num2;
 }
 
 double dzielenie(double num1, double num2) {
     if (num2 != 0) {
-        return num1 * num2;
+        return num1 / num2;
     } else {
         throw "Error! Dividing by 0 is not possible!";
     }
